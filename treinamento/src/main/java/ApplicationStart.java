7ce4d57dca3f8c9f939a1134c11b5381b01da830
Alanis2 <alanisemanuela950@gmail.com>
2021-10-03 19:10:31 -0300

    Terceira-aula

--- treinamento/src/main/java/ApplicationStart.java
+++ treinamento/src/main/java/ApplicationStart.java
@@ -1,11 +1,8 @@
-package start;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
-//@ComponentScan(basePackages = "Pacote")
 public class ApplicationStart {
     public static void main(String[] args){
         SpringApplication.run(ApplicationStart.class, args);
--- treinamento/src/main/java/controller/AnimeController.java
+++ treinamento/src/main/java/controller/AnimeController.java
@@ -1,18 +1,27 @@
 package controller;
 
 import domain.Anime;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import util.DateUtil;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("anime")
+@Log4j2
+@AllArgsConstructor
 public class AnimeController {
-
+    private DateUtil dateUtil;
+    //localhost:8080/anime/list
     @GetMapping(path = "list")
     public List<Anime> list(){
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return List.of(new Anime("DBZ"), new Anime("Berserk"));
     }
 }
