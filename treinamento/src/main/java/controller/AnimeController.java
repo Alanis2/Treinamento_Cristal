fbfb85a638045b0ab3838e71701783d4c7e3959c
Alanis2 <alanisemanuela950@gmail.com>
2021-10-03 22:10:19 -0300

    Aula-seis

--- treinamento/src/main/java/controller/AnimeController.java
+++ treinamento/src/main/java/controller/AnimeController.java
@@ -2,32 +2,28 @@ package controller;
 
 import domain.Anime;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import service.AnimeService;
 import util.DateUtil;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("anime")
+@RequestMapping("animes")
 @Log4j2
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class AnimeController {
-    private DateUtil dateUtil;
-    //localhost:8080/anime/list
-    @GetMapping(path = "list")
+    private final DateUtil dateUtil;
+    private final AnimeService animeService;
+
     public List<Anime> list(){
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("DBZ"), new Anime("Berserk"));
-    }
-
-    @GetMapping(path = "list2")
-    public List<Anime> list2(){
-        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("Ola"), new Anime("Manu"));
+        return animeService.listAll();
     }
 }
---
+++ treinamento/src/main/java/repository/AnimeRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import domain.Anime;
+
+import java.util.List;
+
+public interface AnimeRepository {
+    List<Anime> listAll();
+}
